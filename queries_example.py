c986d910a52e782e6f5d95fc83138808feb932ba
Benjamin Roth <benjaminroth@Benjamins-MBP.hsd1.ca.comcast.net>
2021-07-19 13:11:22 -0700

    example

---
+++ queries_example.py
@@ -0,0 +1,5 @@
+##example of how I structure my queries files (mlb_queries.py, nba_queries.py, etc.)
+#each query is structured like this: (link to the sportsdatabase website of that query, text of the query, play for that query (On, Against, Over, Under, etc.))
+queries = [
+    ('https://sportsdatabase.com/mlb/query?output=default&su=1&ou=1&sdql=H&submit=++S+D+Q+L+%21++', 'H', 'On')
+]
