8cc23470a3db99178f79e8ce452e2e89383cf8cc
benroth8 <37677064+benroth8@users.noreply.github.com>
2021-11-29 10:32:06 -0800

    Update queries_example.py

--- queries_example.py
+++ queries_example.py
@@ -1,5 +1,5 @@
-##example of how I structure my queries files (mlb_queries.py, nba_queries.py, etc.)
-#each query is structured like this: (link to the sportsdatabase website of that query, text of the query, play for that query (On, Against, Over, Under, etc.))
-queries = [
+# example of how I structure my queries files (nba_queries.py, etc.) You have to keep the same number of fields in the query or you'll have to change the indexing in scrapeData.py
+# each query is structured like this: (link to the s3 URL of that query, text of the query, play for that query (On, Against, Over, Under, etc.))
+nbas3queries = [
     ('https://sportsdatabase.com/mlb/query?output=default&su=1&ou=1&sdql=H&submit=++S+D+Q+L+%21++', 'H', 'On')
 ]
